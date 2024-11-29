aac475fd83d8a355ab4377fa324fbfd546b81459
viktorhollanders <viktorhollanders@hey.com>
2024-11-29 10:43:38 +0000

    Add favicon

---
+++ src/components/AddButton.js
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+export default function ButtonAdd() {
+  return (
+    <View>
+      <Image source={require('../../assets/icons/add.png')} />
+    </View>
+  );
+}
--- src/components/boardCard.js
+++ src/components/boardCard.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import { Text, View, Image } from 'react-native';
+
+export default function BoardCard() {
+  return (
+    <View>
+      <Text>hello</Text>
+    </View>
+  );
+}
