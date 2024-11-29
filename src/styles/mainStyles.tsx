dfda95bd2af5866fcc4612640d9a45096a065c8a
viktorhollanders <viktorhollanders@hey.com>
2024-11-29 09:48:13 +0000

    Add src folder

---
+++ src/styles/mainStyles.tsx
@@ -0,0 +1,32 @@
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+export const mainStyles = {
+  fonts: {
+    xs: 12,
+    sm: 16,
+    md: 20,
+    xl: 23,
+  },
+  fontWeights: {
+    regular: '400',
+    semiBold: '500',
+    bold: '800',
+  },
+  colors: {
+    primaryColor: '#00AC5F',
+    secondaryColor: '#00ED82',
+    background: '#FDFDFD',
+
+    white: '#FFFFFF',
+    gray50: '#CCCCCC',
+    gray100: '#919191',
+    gray300: '#5E5E5E',
+    black: '#1F1F1F',
+
+    // Action colors
+    success: '#00E649',
+    error: '#FF5656',
+  },
+  borderRadius: 5,
+  boxShadow: `0px 0px 14px 0px #D0D0D0`,
+};
